762ce23f66b83ba418005a1be73ac5b38825e2fa
progforce <alexander.ab@progforce.com>
2013-10-21 18:58:36 +0400

    Initial Commit

---
+++ IMDB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IMDB
+//
+//  Created by progforce on 21.10.13.
+//  Copyright (c) 2013 Progforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IMDB/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  IMDB
+//
+//  Created by progforce on 21.10.13.
+//  Copyright (c) 2013 Progforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
